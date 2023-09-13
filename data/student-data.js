dc6585d457bf373ecd22c6dbccaa2f0bb97700e6
Enes <enesvelovic@gmail.com>
2023-09-13 01:23:27 -0400

    Lab Complete

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Enes Velovic', age: 35, _id: 125223},
+  {name: 'Jasmina Arucevis', age: 39, _id: 127904},
+  {name: 'Sevdiana Dzaferovic', age: 37, _id: 139608},
+]
+
+export { 
+	students
+}
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 // import modules
 
 import express from 'express'
-
+import { students } from './data/student-data.js'
 // create Express app
 
 const app = express()
 
 // configure the app (app.set)
 
-
+app.set('view engine', 'ejs')
 
 // mount Middleware (app.use)
 
@@ -16,10 +16,18 @@ const app = express()
 
 // Mount routes
 app.get('/', function(req, res) {
-  res.send('<h1>Hello Express</h1>')
+  res.redirect('/students')
 })
 
+app.get('/home', function(req, res) {
+  res.render('home')
+})
 
+app.get('/students', function(req, res) {
+  res.render('students/index', {
+    students
+  })
+})
 
 // tell the app to listen on port 3000
 
